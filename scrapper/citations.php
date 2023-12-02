c593d84f7d753433239ea598d00741b6d56ba32f
Joao Coelho <jota.pedro.coelho@gmail.com>
2023-12-02 01:42:36 +0100

    no message

---
+++ scrapper/citations.php
@@ -0,0 +1,73 @@
+<?php
+
+$docs = getArxivDocs();
+
+$directory = '/Volumes/Alpha/data/citation_metadata/csv';
+$papers = parseCsvFiles($directory, $docs);
+print_r($papers);
+
+function getArxivDocs() {
+  $filename = './data/docs-with-omid.jsons';
+  $handle = fopen($filename, "r");
+  $docs = [];
+
+  if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+      $decodedLine = json_decode($line, true);
+  
+      if ($decodedLine === null) {
+        echo "Error decoding JSON: " . json_last_error_msg() . "\n";
+      } else {
+        $docs[$decodedLine['omid']] = $decodedLine;
+      }
+    }
+  }
+
+  return $docs;
+}
+
+
+function parseCsvFiles($directory, $docs) {
+  $papers = [];
+
+  // Find all CSV files in the specified directory
+  $csvFiles = glob($directory . '/*.csv');
+
+  $i = 0;
+  $fileCount = count($csvFiles);
+
+  foreach ($csvFiles as $file) {
+      echo 'File ' . $i . '/' . $fileCount . PHP_EOL;
+      $paperCount = 0;
+
+      // Open the CSV file
+      if (($handle = fopen($file, "r")) !== FALSE) {
+          while (($data = fgetcsv($handle, 10000, ",")) !== FALSE) {
+              // Extract title and omid
+              $citing = $data[1] ?? '';
+              $cited = $data[2] ?? '';
+              $isJournal = $data[5] ?? '';
+              $isAuthor = $data[6] ?? '';
+
+              if (isset($docs[$cited])) {
+                $docs[$cited]['citing'][] = $citing;
+                echo $cited . ' -> ' . $citing . PHP_EOL;
+              }
+
+              $paperCount++;
+
+              if ($paperCount % 1000000 === 0) {
+                echo 'Reference: ' . $paperCount . PHP_EOL;
+              }
+          }
+
+          fclose($handle);
+      }
+
+      $i++;
+  }
+
+  return $papers;
+}
+
+?>
